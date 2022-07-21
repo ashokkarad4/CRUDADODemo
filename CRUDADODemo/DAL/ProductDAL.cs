0466a3421ab0b320db2ffaf9a4997df76f91da4d
ashokkarad4 <ashokkarad4@gmail.com>
2022-07-21 12:47:57 +0530

    Add project files.

---
+++ CRUDADODemo/DAL/ProductDAL.cs
@@ -0,0 +1,99 @@
+﻿using CRUDADODemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDADODemo.DAL
+{
+    public class ProductDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public ProductDAL()
+        {
+            con = new SqlConnection(Startup.ConnectinString);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> plist = new List<Product>();
+            string qry = "select * from product";
+            cmd = new SqlCommand(qry, con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Product p = new Product();
+                    p.Id = Convert.ToInt32(dr["Id"]);
+                    p.Name = dr["Name"].ToString();
+                    p.Salary = Convert.ToDouble(dr["Price"]);
+                    plist.Add(p);
+                }
+            }
+            con.Close();
+            return plist;
+        }
+        public Product GetProductById(int id)
+        {
+            Product p = new Product();
+            string qry = "select * from product where Id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    p.Id = Convert.ToInt32(dr["Id"]);
+                    p.Name = dr["Name"].ToString();
+                    p.Salary = Convert.ToDouble(dr["Price"]);
+                }
+            }
+            con.Close();
+            return p;
+        }
+
+        public int AddProduct(Product prod)
+        {
+            string qry = "insert into product values(@name,@price)";
+            cmd = new SqlCommand(qry, con);
+
+            cmd.Parameters.AddWithValue("@name", prod.Name);
+            cmd.Parameters.AddWithValue("@price", prod.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int UpdateProduct(Product prod)
+        {
+            string qry = "update product set Name=@name , Price=@price where Id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", prod.Id);
+            cmd.Parameters.AddWithValue("@name", prod.Name);
+            cmd.Parameters.AddWithValue("@price", prod.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int DeleteProduct(int id)
+        {
+            string qry = "delete from product where Id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+    }
+
+}
+
---
+++ CRUDADODemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDADODemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
